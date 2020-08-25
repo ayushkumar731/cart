0c44f3e0b21546e63bf57590f402d81135bf694a
Ayush Kumar <kumarayush731@gmail.com>
2020-08-25 09:29:11 +0530

    total price

--- src/App.js
+++ src/App.js
@@ -11,21 +11,24 @@ class App extends React.Component {
           price: 99,
           title: 'Watch',
           qty: 1,
-          img: '',
+          img:
+            'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60',
           id: 1,
         },
         {
           price: 999,
           title: 'Mobile Phone',
           qty: 10,
-          img: '',
+          img:
+            'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1001&q=80',
           id: 2,
         },
         {
           price: 999,
           title: 'Laptop',
           qty: 4,
-          img: '',
+          img:
+            'https://images.unsplash.com/photo-1548611635-b6e7827d7d4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60',
           id: 3,
         },
       ],
@@ -77,6 +80,15 @@ class App extends React.Component {
     return count;
   };
 
+  getCartTotal = () => {
+    const { products } = this.state;
+    let cartTotal = 0;
+    products.forEach(
+      (product) => (cartTotal = cartTotal + product.qty * product.price),
+    );
+    return cartTotal;
+  };
+
   render() {
     const { products } = this.state;
     return (
@@ -88,6 +100,9 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProducts={this.handleDeleteProducts}
         />
+        <div style={{ padding: 10, fontSize: 20 }}>
+          TOTAL:{this.getCartTotal()}
+        </div>
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -42,7 +42,7 @@ const CartItem = (props) => {
   return (
     <div className="cart-item">
       <div className="left-block">
-        <img style={style.image} />
+        <img style={style.image} src={product.img} />
       </div>
       <div className="right-block">
         <div style={{ fontSize: 25 }}>{title}</div>
